66f4d83e7cd1412d024bc073a4d88c706ffcf527
Jean Carrillo <jeancarrillo@gmail.com>
2019-04-09 09:50:01 +0200

    first matrix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const matrix = [
+      ['0', '0', '0'],
+      ['0', '0', '0'],
+      ['0', '0', '0']
+    ]
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          matrix.map((row, rowIndex) => {
+            return row.map((col, colIndex)=>{
+              return <span>{matrix[rowIndex][colIndex]}</span>
+            })
+          })
+        }
       </div>
     );
   }
