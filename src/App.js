6034df5f8dc1a64542e1231061ffe8ffb1971109
Jean Carrillo <jeancarrillo@gmail.com>
2019-04-09 10:32:15 +0200

    matriiix

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    const matrix = [
-      ['0', '0', '0'],
-      ['0', '0', '0'],
-      ['0', '0', '0']
-    ]
-    return (
-      <div className="App">
+  constructor(props){
+    super(props)
+    this.state={
+      activeChar: [0,0],
+      matrix : []
+    }
+  }
 
+  componentDidMount(){
+    let matrix = []
+    for (let i = 0; i < 30;i++){
+      matrix.push([])
+      for (let j=0; j< 50; j++){
+        matrix[i].push('0')
+      }
+    }
+    this.setState({
+      matrix
+    })
+  }
+  getRandomPosition(){
+  }
+
+  
+  render() {
+    return (
+      <div className="App" style={{backgroundColor: 'black'}}>
         {
-          matrix.map((row, rowIndex) => (
-            <p key={rowIndex}>
+          this.state.matrix.map((row, rowIndex) => (
+            <p style={{lineHeight:'0.3em'}} key={rowIndex}>
               {
                 row.map((char, colIndex) => (
-                  <span key={colIndex}>{char}</span>
+                  <span style={{color: 'green'}} key={colIndex}>{char}</span>
                 ))
               }
             </p>
